6c7757df47769d139eb9be259f2cc2a9a8f3756f
olgareuts <olga.re@mail.ru>
2023-04-28 23:52:57 +0300

    feat: add button click animation

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
+
+const keys = ['~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '&larr;', 
+              'Tab','Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\',  
+              'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", '&crarr;',
+              '&uarr;', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '&#9650;','&uarr;',
+              'option', 'CMD', '' , 'CMD', 'option','&#9668;', '&#9660;', '&#9658;','Ctrl'];
+const keysCode = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace',
+                  'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash',
+                  'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter',
+                  'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight',
+                  'AltLeft', 'MetaLeft', 'Space', 'MetaRight', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlLeft']
+
 const container = document.createElement('div');
 const keyboard = document.createElement('div');
 const input = document.createElement('textarea');
-const keys = ['~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '&larr;', 
-              'Tab','Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'DEL', 
-              'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", '&crarr;',
-              '&uarr;', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '&#9650;','&uarr;',
-               'Alt', 'Ctrl', '' , 'Ctrl', 'Alt', '&#9668;', '&#9660;', '&#9658;','fn'];
-const data = {};
+const subtitle = document.createElement('span');
+subtitle.innerHTML = 'Клавиатура создана в операционной системе macOS';
+input.autofocus = true;
 document.body.append(container);
 container.className = 'wrapper';
 keyboard.className = 'keyboard';
 input.className = 'input';
 container.append(input);
 container.append(keyboard);
+container.append(subtitle);
+
 
 const addKey = (j, i) => {
   let key = document.createElement('div');
@@ -30,27 +41,48 @@ let rows = document.getElementsByClassName('row');
   for (i = 0; i < 14; i++) {
     rows[0] = addKey(i, 0);
   }
-  for ( i = 14; i < 29; i++) {
+  for ( i = 14; i < 28; i++) {
     rows[1] = addKey(i, 1);
   }
-  for (i = 29; i < 42; i++) {
+  for (i = 28; i < 41; i++) {
     rows[2] = addKey(i, 2);
   }
-  for (i = 42; i < 55; i++) {
+  for (i = 41; i < 54; i++) {
     rows[3] = addKey(i, 3);
   }
-  for (i = 55; i < 64; i++) {
+  for (i = 54; i < 63; i++) {
     rows[4] = addKey(i, 4);
   }
 
 let keyboardKeys = document.getElementsByClassName('key');
 keyboardKeys[13].classList.add('key_medium');
 keyboardKeys[14].classList.add('key_medium');
-keyboardKeys[29].classList.add('key_medium');
+keyboardKeys[28].classList.add('key_medium');
+keyboardKeys[40].classList.add('key_medium');
 keyboardKeys[41].classList.add('key_medium');
-keyboardKeys[42].classList.add('key_medium');
-keyboardKeys[54].classList.add('key_medium');
-keyboardKeys[57].classList.add('key_long');
+keyboardKeys[53].classList.add('key_medium');
+keyboardKeys[56].classList.add('key_long');
+keyboardKeys[54].classList.add('key_small');
+keyboardKeys[55].classList.add('key_small');
+keyboardKeys[57].classList.add('key_small');
+keyboardKeys[58].classList.add('key_small');
+
+document.addEventListener('keydown', function(event) {
+  if (keysCode.includes(event.code)) {
+    let pos = keysCode.indexOf(event.code);
+    keyboardKeys[pos].classList.add('key_active');
+  }
+});
+
+document.addEventListener('keyup', function(event) {
+  if (keysCode.includes(event.code)) {
+    let pos = keysCode.indexOf(event.code);
+    keyboardKeys[pos].classList.remove('key_active');
+  }
+  console.log(event.code)
+});
+
+
 
 
 
